930dcbdc2c3977469529c503e31bc2e0f28a248d
dyune <david.wang5@mail.mcgill.ca>
2025-02-01 11:48:33 -0500

    Hello world

---
+++ conuhax/src/main/java/com/proj/conuhax/ConuhaxApplication.java
@@ -0,0 +1,13 @@
+package com.proj.conuhax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConuhaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConuhaxApplication.class, args);
+    }
+
+}
---
+++ conuhax/src/test/java/com/proj/conuhax/ConuhaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.conuhax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConuhaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
