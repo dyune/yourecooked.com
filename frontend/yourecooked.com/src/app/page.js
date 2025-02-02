26ad51187a8ca33ea476b1e4d50461e129f6bc87
Tyler Vuong <tylervuong2004@gmail.com>
2025-02-01 23:12:10 -0500

    added dashboard and its leaderboard

--- frontend/yourecooked.com/src/app/page.js
+++ frontend/yourecooked.com/src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
-
+import Head from "next/head";
+import { Button } from "@/components/ui/button"
+import { Dashboard } from "@/components/ui/dashboard";
 export default function Home() {
   return (
-
+    <div>
+      <Dashboard />
+    </div>
 
   );
 }
