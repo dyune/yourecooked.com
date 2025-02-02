7f66e6daaa91b375fb0dae7ccfc684e39d68f969
Tyler Vuong <tylervuong2004@gmail.com>
2025-02-02 00:05:31 -0500

    fixed more merge issues

--- frontend/yourecooked.com/src/app/page.js
+++ frontend/yourecooked.com/src/app/page.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import Image from "next/image";
 import Head from "next/head";
 import { Button } from "@/components/ui/button"
 import { Dashboard } from "@/components/ui/dashboard";
+import { TableDemo } from "@/components/ui/applications";
+import { ButtonDemo } from "@/components/ui/applications";
 export default function Home() {
   return (
     <div>
-      <Dashboard />
-=======
-import {ApplicationPage} from "../components/ui/applications";
-import {TableDemo} from "../components/ui/applications";
-import {ButtonDemo} from "../components/ui/applications";
-
-
-export default function Home() {
-  return (
-    <div>
-      <ButtonDemo/>
-      <TableDemo/>
->>>>>>> celine-branch
+      <ButtonDemo />
+      <TableDemo />
     </div>
 
   );
